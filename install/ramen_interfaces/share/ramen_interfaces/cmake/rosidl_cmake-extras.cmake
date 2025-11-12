3e6f756048e03b800576f5b74dd1a63dc7a642c7
jiyoung1216 <jiyoung03355@gmail.com>
2025-11-12 23:44:16 +0900

    Delete install directory
     필요없

--- install/ramen_interfaces/share/ramen_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(ramen_interfaces_IDL_FILES "msg/RamenOrder.idl;msg/CookedRamen.idl;action/ServeRamen.idl;action/RequestRemake.idl")
-set(ramen_interfaces_INTERFACE_FILES "msg/RamenOrder.msg;msg/CookedRamen.msg;action/ServeRamen.action;action/RequestRemake.action")
