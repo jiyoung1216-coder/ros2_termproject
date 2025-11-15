5c3b095c22b6f20119dadc63cdacc54994aef7bb
jiyoung1216 <jiyoung03355@gmail.com>
2025-11-15 15:29:24 +0900

    Update setup.py

--- src/ramen_table/setup.py
+++ src/ramen_table/setup.py
@@ -21,7 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'order_node = ramen_table.order_node:main',
-            'order1_node = ramen_table.order_node1:main',
+            'order1_node = ramen_table.order1_node:main',
             'customer_node = ramen_table.customer_node:main',
         ],
     },
